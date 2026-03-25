7917f739a7d30745bd29776cf3e99469f98d56f7
Roy Zeng <142411359+RoyZeng0317@users.noreply.github.com>
2026-03-25 15:16:01 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // define default response
 const response = {
  "": "我是機器人，有什麼我可以幫助你的?",
- "": "",
+ "請問我想要找有關計算機的網站": "以下是有關計算機的網站",
  "": ""
 };
 function send(){
  let input = document.getElementById("");
 
+}
+// chatbot response
+if(enter include =""){
 }
