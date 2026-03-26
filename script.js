f188b7e95bf205ea1107c7aa4fd1fd1059f09427
Roy Zeng <142411359+RoyZeng0317@users.noreply.github.com>
2026-03-26 20:58:55 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -20,9 +20,19 @@ const keyword = [
     }
 ]
 // send the message
-function send(msg){
-    let input = document.getElementById("");
-    msg = msg.toLowerCase();
+function send(){
+    let input = document.getElementById("msg");
+    let msg = input.value;
+
+    if(msg === "") return;
+
+    addMessage("You: " +msg);
+
+    let reply = response(msg);
+    addMessage("Boot: " + reply);
+
+    input.value = "";
+    // msg = msg.toLowerCase();
 
     for(let rule of rules){
         for(let key of rule.keywords){
@@ -33,12 +43,24 @@ function send(msg){
     }
     return "Soory, I can't understand your meaning.";
 }
+// display the message
+function addMessage(text){
+    let log = document.getElementById("chat-log");
+    log.innerHTML += "<div>" + text + "</div>";
+    log.scrollTop = log.scrollHeight;
+}
+// key reply
 
 document.getElementById("msg").addEventListener("keydown", function(e){
     if(e.key === "Enter"){
         sendMessage();
     }
 })
+// open the chat
 function openChat(){
-
+    document.getElementById("chatWindow").style.display = "block";
+}
+// close the chat
+function closeChat(){
+    document.getElementById("chatWindow").style.display = "none";
 }
